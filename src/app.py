2ebe9bac0a152aa4a9c279e8ede377c91b8772f0
jsegmar0703 <joaquinisleta14@gmail.com>
2025-02-11 13:59:30 +0100

    Update app.py
    nueva ruta

--- src/app.py
+++ src/app.py
@@ -8,5 +8,9 @@ app = Flask(__name__)
 def hello():
     return "Hello, World!"
 
+@app.route('/pecorino')
+def pecorino():
+    return "Hello, World!"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=PORT)
